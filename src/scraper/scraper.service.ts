e595cd2e52c2b4bd4e6bbfd113fa72f5249651c6
Phuoc Nguyen <phuocnguyen@Phuocs-MacBook-Pro.local>
2024-07-30 23:40:57 +0700

    can crawl data

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -54,18 +54,47 @@ export class ScraperService {
           baseUrl: domain,
         });
 
-        if (!request.loadedUrl.endsWith('/items/')) {
+        if (!request.loadedUrl.endsWith('/items')) {
           try {
             await page.waitForSelector('table.items-table tr', { timeout: 5000 });
 
-            const rowCount = await page.$$eval('table.items-table tr', rows => rows.length);
+            const rowCount = await page.$$eval('table.items-table tr', rows => {
+              return rows.slice(1).map(row => {
+                const cells = row.querySelectorAll('td');
+                const imageElement = cells[0]?.querySelector('img');
+                const nameElement = cells[1]?.querySelector('div.name.quality3');
+                const subtypesElement = cells[1]?.querySelector('div.subtypes');
+                const descriptionElement = cells[2];
 
+                if (!imageElement || !nameElement || !subtypesElement || !descriptionElement) {
+                  console.error('Missing element:', { imageElement, nameElement, subtypesElement, descriptionElement });
+                  return null;
+                }
+
+                const imageSrc = imageElement.src;
+                const name = nameElement.textContent.trim();
+                const subtypes = subtypesElement.textContent.trim();
+                const description = descriptionElement.textContent.trim();
+                const rankMatch = nameElement.className.match(/quality(\d)/);
+                const rank = rankMatch ? parseInt(rankMatch[1], 10) : null;
+                console.log(name + ' ' + subtypes + ' ' + description + ' ' + rank);
+
+                return {
+                  imageSrc,
+                  name,
+                  subtypes,
+                  description,
+                  rank,
+                };
+              }).filter(item => item !== null); // Filter out null items
+            });
             console.log(`Data extracted from ${request.loadedUrl}:`, rowCount);
           } catch (error) {
             log.error(`Error processing ${request.loadedUrl}: ${error.message}`);
           }
         }
       },
+      headless: false,
     });
     await crawler.run(['https://wuthering.gg/items/']);
   }
