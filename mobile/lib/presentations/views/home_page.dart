db1f49734e6bf310f5c230749b3c6d1b045250ab
Phuoc Nguyen <phuocnguyen@Phuocs-MacBook-Pro.local>
2024-08-20 03:51:04 +0700

    aaa

--- mobile/lib/presentations/views/home_page.dart
+++ mobile/lib/presentations/views/home_page.dart
@@ -12,7 +12,6 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     TextTheme textTheme = Theme.of(context).textTheme;
 
     void switchBranch(BuildContext context, int index) {
@@ -27,117 +26,88 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        // 
-        title:  RichText(
-          text: TextSpan(
-            children: [
-              TextSpan(
-                  text: 'Wuthering',
-                  style: textTheme.titleLarge
-              ),
-              TextSpan(
-                text: ' Tools',
-                style: textTheme.titleLarge!.copyWith(
-                    color: Colors.yellowAccent
-                ),
-              ),
-            ],
-          ),
-        )
-      ),
-      body: Center(
-
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          child: GridView(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+          //
+          title: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(text: 'Wuthering', style: textTheme.titleLarge),
+            TextSpan(
+              text: ' Tools',
+              style: textTheme.titleLarge!.copyWith(color: Colors.yellowAccent),
             ),
-            children: [
-              Card(
-                child: InkWell(
-                  onTap: () {
-                    context.goNamed(Routes.character);
-                  },
-                  child:  Center(
-                    child: Stack(
-                      children: [
-                        CachedNetworkImage(
-                          height: 100,
-                          width: 100,
-                          imageUrl: 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_F_01.png',
-                        ),
-                        const Text('Character'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                child: InkWell(
-                  onTap: () {
-                    // context.goNamed(Routes.character);
-                    switchBranch(context, 0);
-                  },
-                  child:  Center(
-                    child: Stack(
-                      children: [
-                        CachedNetworkImage(
-                          height: 100,
-                          width: 100,
-                          imageUrl: 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_B_03.png',
-                        ),
-                        const Text('Echoes'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                child: InkWell(
-                  onTap: () {
-                    // context.goNamed(Routes.character);
-                    switchBranch(context, 1);
-                  },
-                  child:  Center(
-                    child: Stack(
-                      children: [
-                        CachedNetworkImage(
-                          height: 100,
-                          width: 100,
-                          imageUrl: 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_K_03.png',
-                        ),
-                        const Text('Weapons'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                child: InkWell(
-                  onTap: () {
-                    context.goNamed(Routes.character);
-                    switchBranch(context, 3);
-                  },
-                  child:  Center(
-                    child: Stack(
-                      children: [
-                        CachedNetworkImage(
-                          height: 100,
-                          width: 100,
-                          imageUrl: 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_H_01.png',
-                        ),
-                        const Text('Items'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
+          ],
+        ),
+      )),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
           ),
-        )
+          children: [
+            Card(
+              child: InkWell(
+                onTap: () {
+                  context.goNamed(Routes.character);
+                },
+                child: buildCard(
+                    textTheme,
+                    'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_F_01.png',
+                    'Characters'),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () {
+                  // context.goNamed(Routes.character);
+                  switchBranch(context, 0);
+                },
+                child: buildCard(textTheme,
+                    'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_B_03.png', 'Echoes'),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () {
+                  // context.goNamed(Routes.character);
+                  switchBranch(context, 1);
+                },
+                child: buildCard(textTheme, 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_K_03.png', 'Weapons'),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () {
+                  context.goNamed(Routes.character);
+                  switchBranch(context, 3);
+                },
+                child: buildCard(textTheme, 'https://wuthering.gg/_ipx/q_70&s_200x200/images//IconChatEmoticon/T_ChatEmo_H_01.png', 'Items'),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+  Center buildCard(TextTheme textTheme, String imageUrl, String title) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CachedNetworkImage(
+            height: 120,
+            width: 120,
+            imageUrl: imageUrl,
+          ),
+          Text(
+            title,
+            style: textTheme.titleMedium,
+          ),
+        ],
       ),
     );
   }
